231e41da5b1a40b203db4717a426d4d7660d6fb5
unknown <anwarmusthafa99@gmail.com>
2024-12-04 23:44:33 +0530

    update main.yml

--- celeryapp/settings.py
+++ celeryapp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.89.215.151','0.0.0.0']
 
 
 # Application definition
