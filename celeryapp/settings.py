0b48b4a683a6ab1d36910dd838c52e32a4c3b871
unknown <anwarmusthafa99@gmail.com>
2024-12-04 19:50:37 +0530

    fix: static folder configuration

--- celeryapp/settings.py
+++ celeryapp/settings.py
@@ -13,6 +13,7 @@ from __future__ import absolute_import, unicode_literals
 from pathlib import Path
 from celery import Celery
 from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,7 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
